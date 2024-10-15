471c49be543254973c32871c9918c3a2bd89e8bf
takuseno <takuma.seno@gmail.com>
2024-10-16 00:16:49 +0900

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  site: 'https://takuseno.github.io',
+  base: '/nota-lp',
 });
